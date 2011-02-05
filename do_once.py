469bd5cae9ef7a030bf05aee9c58fa44887d33dd
Matty G <meawoppl@gmail.com>
2011-02-04 23:16:05 -0600

    Added debug only printing, and removed stupid try-except on do over input.

--- do_once.py
+++ do_once.py
@@ -1,3 +1,31 @@
+# Do Once Function Decorator
+# Copyright (c) 2011, Matthew Goodman
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#    This product includes software developed by the <organization>.
+# 4. Neither the name of the <organization> nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import os, numpy, pickle, hashlib
 
 class DoOnce:
@@ -6,10 +34,8 @@ class DoOnce:
         print "In Init"
 
     def __call__(self, *args, **kwargs):
-        try:
-            doover = kwargs.pop("doover")
-        except KeyError:
-            doover = False
+        doover = kwargs.pop("do_over", False)
+        debug  = kwargs.pop("do_debug", False)
 
         md5 = hashlib.md5()
             
@@ -31,24 +57,24 @@ class DoOnce:
         # Look for an already computed the result
         # We dont know if it will be a npy or pkl so check both
         if os.path.isfile(pkl_file) and doover == False:
-            print "Previous PKL found:", pkl_file
+            if debug: print "Previous PKL found:", pkl_file
             return pickle.load(open(pkl_file))
         if os.path.isfile(npy_file) and doover == False:
-            print "Previous NPY found:", npy_file
+            if debug: print "Previous NPY found:", npy_file
             return numpy.load(npy_file)
         print "No previous result found, or redoing. . ."
 
         # I guess we didnt compute the result, or we are recomputing it . . .
-        print "Computing Result . . ."
+        if debug: print "Computing Result . . ."
         res = self.f(*args, **kwargs)
         
         # If it is an ndarray, save it that way, otherwise . . . pickle it!
         if isinstance(res, numpy.ndarray):
-            print "Saving numpy array", npy_file
+            if debug: print "Saving numpy array", npy_file
             flo = open(npy_file, "w")
             numpy.save(flo, res)
         else:
-            print "Saving pickle array", pkl_file
+            if debug: print "Saving pickle array", pkl_file
             flo = open(pkl_file, "w")
             pickle.dump(res, flo)
         # Flush and close the file object
